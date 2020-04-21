89a7989bd9350e8ebff3ff8da8a3a8ad974c6bd1
Tim Anema <tim.anema@hotmail.nl>
2020-04-21 16:10:19 +0200

    Added grpc server and connection information

--- services/control_plane/main.go
+++ services/control_plane/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"emulating-k8s/api/heartbeat"
+	"emulating-k8s/pkg/service"
 	"golang.org/x/net/context"
 	"log"
 )
@@ -9,11 +10,16 @@ import (
 func main()  {
 	log.Println("Starting Apate control plane")
 
+	// Connection settings
+	connectionInfo := service.NewConnectionInfo("localhost", 8080, true)
+
 	// Server
-	heartbeat.StartServer()
+	server := service.NewGRPServer(connectionInfo)
+	heartbeat.RegisterService(server)
+	server.Serve()
 
 	// Client
-	c := heartbeat.GetClient()
+	c := heartbeat.GetClient(connectionInfo)
 	defer c.Conn.Close()
 
 	res, err := c.Client.Ping(context.Background(), &heartbeat.HeartbeatMessage{Message: "ping"})
