6c2456822f67f0c62327f02ac36ba1e401d57df8
jdonszelmann <j.b.donszelmann@student.tudelft.nl>
2020-04-24 10:36:39 +0200

    fixed memory and time desugaring

--- pkg/scenario/normalise/timestamp.go
+++ pkg/scenario/normalise/timestamp.go
@@ -1,10 +1,7 @@
 package normalise
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
-	"unicode"
+	"time"
 )
 
 // Takes a timestamp as given in a public.scenario and desugars it into an integer milliseconds.
@@ -15,80 +12,11 @@ import (
 //		- <int>ms				Milliseconds after the start of the scenario
 // 		- <int>m				Minutes after the start of the scenario
 //		- <int>h				Hours after the start of the scenario
-func desugarTimestamp(time string) (int, error) {
-	// First remove all leading and trailing spaces
-	time = strings.TrimSpace(time)
-
-	// postfix 1 and 2
-	p1 := unicode.ToLower(rune(time[len(time)-1]))
-	p2 := unicode.ToLower(rune(time[len(time)-2]))
-
-	var v int
-
-	switch p1 {
-	case 's':
-		switch p2 {
-		case 'm':
-			// Milliseconds (take everything before the second to last char and convert to int)
-
-			iv, err := strconv.ParseInt(time[:len(time)-2], 10, 64)
-			if err != nil {
-				return 0, err
-			}
-
-			v = int(iv)
-
-		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.':
-			// I match on numbers or . here (. for floats) to be more descriptive in error messages.
-			// Otherwise invalid postfixes would result in integer conversion errors.
-			// Seconds (take everything before the last char and convert to float)
-
-			fv, err := strconv.ParseFloat(time[:len(time)-1], 64)
-			if err != nil {
-				return 0, err
-			}
-
-			// multiply by 1000 to go to milliseconds, then round to the nearest ms
-			v = int(fv * 1000)
-
-		default:
-			return 0, fmt.Errorf("couldn't decode postfix. Possible values are ms, s, m, h or no postfix (%s)", time)
-		}
-	case 'm':
-		// Minutes (take everything before the last char and convert to int)
-
-		iv, err := strconv.ParseInt(time[:len(time)-1], 10, 64)
-		if err != nil {
-			return 0, err
-		}
-
-		// multiply by 1000 * 60 to go to milliseconds.
-		v = int(iv * 60 * 1000)
-	case 'h':
-		// Minutes (take everything before the last char and convert to int)
-
-		iv, err := strconv.ParseInt(time[:len(time)-1], 10, 64)
-		if err != nil {
-			return 0, err
-		}
-
-		// multiply by 1000 * 60 * 60 to go to milliseconds.
-		v = int(iv * 60 * 60 * 1000)
-	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.':
-		// I match on numbers or . here (. for floats) to be more descriptive in error messages.
-		// Otherwise invalid postfixes would result in integer conversion errors.
-		// Seconds (no postfix, take everything and convert to float)
-
-		fv, err := strconv.ParseFloat(time, 64)
-		if err != nil {
-			return 0, err
-		}
-
-		// multiply by 1000 to go to milliseconds, then round to the nearest ms
-		v = int(fv * 1000)
-	default:
-		return 0, fmt.Errorf("couldn't decode postfix. Possible values are ms, s, m, h or no postfix (%s)", time)
+func desugarTimestamp(t string) (int, error) {
+	duration, err := time.ParseDuration(t)
+	if err != nil {
+		return 0, err
 	}
 
-	return v, nil
+	return int(duration.Milliseconds()), nil
 }
