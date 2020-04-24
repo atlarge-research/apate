7657f91436f5364abaf44e4a491d9f2a7e90ff8e
jdonszelmann <j.b.donszelmann@student.tudelft.nl>
2020-04-24 07:45:42 +0200

    moved cli to cmd

--- cli/apate/app/main.go
+++
@@ -1,8 +0,0 @@
-// Package app provides the main cli interface for Apate.
-package app
-
-// Main is the entry point of the cli interface for Apate.
-func Main(_ []string) {
-	// TODO: create logger
-
-}
