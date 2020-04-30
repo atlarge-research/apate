278d40e17278dd2c4ec6c2596174d8439626db50
Robbin Baauw <robbin.baauw@gmail.com>
2020-04-30 12:07:38 +0200

    Converted embeddings to functions

--- pkg/scenario/normalization/events/translator.go
+++ pkg/scenario/normalization/events/translator.go
@@ -20,8 +20,8 @@ func NewEventTranslator(originalTask *controlplane.Task, newTask *apatelet.Task)
 	}
 }
 
-func (th *EventTranslator) createNodeEvent() *NodeEventWrapper {
-	nodeEvent := &apatelet.NodeEvent{
+func (th *EventTranslator) createNodeEvent() *apatelet.NodeEvent {
+	ne := &apatelet.NodeEvent{
 		NodeState: &apatelet.NodeState{
 			NodeLifecycleState: &apatelet.NodeState_NodeLifecycleState{
 				LifecycleState: &apatelet.LifecycleState{},
@@ -30,24 +30,20 @@ func (th *EventTranslator) createNodeEvent() *NodeEventWrapper {
 			AddedLatencyState: &apatelet.NodeState_AddedLatencyState{},
 		},
 	}
-	th.newTask.Event = &apatelet.Task_NodeEvent{NodeEvent: nodeEvent}
-	n := &NodeEventWrapper{nodeEvent: nodeEvent}
-	n.BaseEventWrapper = BaseEventWrapper{lifecycleGetter: n}
-	return n
+	th.newTask.Event = &apatelet.Task_NodeEvent{NodeEvent: ne}
+	return ne
 }
 
-func (th *EventTranslator) createPodEvent() *PodEventWrapper {
-	podEvent := &apatelet.PodEvent{
+func (th *EventTranslator) createPodEvent() *apatelet.PodEvent {
+	pe := &apatelet.PodEvent{
 		PodState: &apatelet.PodState{
 			PodLifecycleState: &apatelet.PodState_PodLifecycleState{
 				LifecycleState: &apatelet.LifecycleState{},
 			},
 		},
 	}
-	th.newTask.Event = &apatelet.Task_PodEvent{PodEvent: podEvent}
-	p := &PodEventWrapper{podEvent: podEvent}
-	p.BaseEventWrapper = BaseEventWrapper{lifecycleGetter: p}
-	return p
+	th.newTask.Event = &apatelet.Task_PodEvent{PodEvent: pe}
+	return pe
 }
 
 func (th *EventTranslator) TranslateEvent() error {
@@ -58,88 +54,92 @@ func (th *EventTranslator) TranslateEvent() error {
 	switch x := th.originalTask.Event.(type) {
 	// Node events
 	case *controlplane.Task_NodeFailure:
-		ne := th.createNodeEvent()
-		ne.applyAction(scenario.LifecycleAction_TIMEOUT, 100)
+		applyNodeAction(th.createNodeEvent(), scenario.LifecycleAction_TIMEOUT, 100)
 
 	case *controlplane.Task_NetworkLatency:
-		latencyState := th.createNodeEvent().nodeEvent.GetNodeState().GetAddedLatencyState()
+		latencyState := th.createNodeEvent().GetNodeState().GetAddedLatencyState()
 		latencyState.AddedLatencyEnabled = true
 		latencyState.AddedLatencyMsec = x.NetworkLatency.GetLatencyMsec()
 
 	case *controlplane.Task_TimeoutKeepHeartbeat:
 		ne := th.createNodeEvent()
-		ne.applyAction(scenario.LifecycleAction_TIMEOUT, 100)
-		ne.setPingAction(scenario.LifecycleAction_NORMAL, 0)
+		applyNodeAction(ne, scenario.LifecycleAction_TIMEOUT, 100)
+		setPingAction(getNodeLifecycleState(ne), scenario.LifecycleAction_NORMAL, 0)
 
 	case *controlplane.Task_NoTimeoutNoHeartbeat:
 		ne := th.createNodeEvent()
-		ne.setPingAction(scenario.LifecycleAction_TIMEOUT, 100)
+		setPingAction(getNodeLifecycleState(ne), scenario.LifecycleAction_TIMEOUT, 100)
 
 	case *controlplane.Task_NodeLifecycleState:
 		ne := th.createNodeEvent()
+		nodeLifecycleState := getNodeLifecycleState(ne)
+		lifecycleState := nodeLifecycleState.GetLifecycleState()
+
 		state := x.NodeLifecycleState
 
 		switch state.Type {
 		case events.LifecycleType_CREATE_POD:
-			ne.setCreatePodAction(state.Action, state.Percentage)
+			setCreatePodAction(lifecycleState, state.Action, state.Percentage)
 
 		case events.LifecycleType_UPDATE_POD:
-			ne.setUpdatePodAction(state.Action, state.Percentage)
+			setUpdatePodAction(lifecycleState, state.Action, state.Percentage)
 
 		case events.LifecycleType_DELETE_POD:
-			ne.setDeletePodAction(state.Action, state.Percentage)
+			setDeletePodAction(lifecycleState, state.Action, state.Percentage)
 
 		case events.LifecycleType_GET_POD:
-			ne.setGetPodAction(state.Action, state.Percentage)
+			setGetPodAction(lifecycleState, state.Action, state.Percentage)
 
 		case events.LifecycleType_GET_POD_STATUS:
-			ne.setGetPodStatusAction(state.Action, state.Percentage)
+			setGetPodStatusAction(lifecycleState, state.Action, state.Percentage)
 
 		case events.LifecycleType_GET_PODS:
-			ne.setGetPodsAction(state.Action, state.Percentage)
+			setGetPodsAction(nodeLifecycleState, state.Action, state.Percentage)
 
 		case events.LifecycleType_PING:
-			ne.setPingAction(state.Action, state.Percentage)
+			setPingAction(nodeLifecycleState, state.Action, state.Percentage)
 		}
 
 	case *controlplane.Task_ResourcePressure:
-		resourceState := th.createNodeEvent().nodeEvent.GetNodeState().GetResourceState()
+		resourceState := th.createNodeEvent().GetNodeState().GetResourceState()
 		resourceState.EnableResourceAlteration = true
 		resourceState.CpuUsage = x.ResourcePressure.GetCpuUsage()
 		resourceState.MemoryUsage = x.ResourcePressure.GetMemoryUsage()
 		resourceState.StorageUsage = x.ResourcePressure.GetStorageUsage()
 		resourceState.EphemeralStorageUsage = x.ResourcePressure.GetEphemeralStorageUsage()
 
-		// Pod events
+	// Pod events
 	case *controlplane.Task_PodLifecycleState:
 		pe := th.createPodEvent()
+		lifecycleState := getPodLifecycleState(pe).GetLifecycleState()
+
 		state := x.PodLifecycleState
 
 		switch state.Type {
 		case events.LifecycleType_CREATE_POD:
-			pe.setCreatePodAction(state.Action, state.Percentage)
+			setCreatePodAction(lifecycleState, state.Action, state.Percentage)
 
 		case events.LifecycleType_UPDATE_POD:
-			pe.setUpdatePodAction(state.Action, state.Percentage)
+			setUpdatePodAction(lifecycleState, state.Action, state.Percentage)
 
 		case events.LifecycleType_DELETE_POD:
-			pe.setDeletePodAction(state.Action, state.Percentage)
+			setDeletePodAction(lifecycleState, state.Action, state.Percentage)
 
 		case events.LifecycleType_GET_POD:
-			pe.setGetPodAction(state.Action, state.Percentage)
+			setGetPodAction(lifecycleState, state.Action, state.Percentage)
 
 		case events.LifecycleType_GET_POD_STATUS:
-			pe.setGetPodStatusAction(state.Action, state.Percentage)
+			setGetPodStatusAction(lifecycleState, state.Action, state.Percentage)
 
 		default:
 			return errors.New("can't alter the GetPods / Ping response on pod level")
 		}
 
 	case *controlplane.Task_PodStatusUpdate:
-		th.createPodEvent().podEvent.GetPodState().PodStatus = x.PodStatusUpdate.NewStatus
+		th.createPodEvent().GetPodState().PodStatus = x.PodStatusUpdate.NewStatus
 
 	case *controlplane.Task_PodStartTimeUpdate:
-		th.createPodEvent().podEvent.GetPodState().StartTime = x.PodStartTimeUpdate.NewStartTime
+		th.createPodEvent().GetPodState().StartTime = x.PodStartTimeUpdate.NewStartTime
 	}
 
 	return nil
