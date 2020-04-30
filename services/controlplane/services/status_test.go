33cd09b9bd2c61830dee00476784c69c66db4583
Vivian Roest <vivian@0x76.dev>
2020-04-30 14:33:42 +0200

    Fixed lints

--- services/controlplane/services/status_test.go
+++ services/controlplane/services/status_test.go
@@ -2,22 +2,22 @@ package services
 
 import (
 	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+
 	"github.com/atlarge-research/opendc-emulate-kubernetes/api/health"
 	"github.com/atlarge-research/opendc-emulate-kubernetes/pkg/service"
 	"github.com/atlarge-research/opendc-emulate-kubernetes/services/controlplane/store"
 	"github.com/atlarge-research/opendc-emulate-kubernetes/services/controlplane/store/mock_store"
-	"github.com/golang/mock/gomock"
-	"github.com/google/uuid"
-	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 //go:generate sh -c "cd ../../../ && make mockgen"
 
 func TestStatusSimple(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	ctrl, ctx := gomock.WithContext(ctx, t)
+	ctrl := gomock.NewController(t)
 
 	// Created mocked store
 	ms := mock_store.NewMockStore(ctrl)
@@ -34,16 +34,13 @@ func TestStatusSimple(t *testing.T) {
 	var s store.Store = ms
 	ss := statusService{&s}
 
-	ret, err := ss.Status(nil, nil)
+	ret, err := ss.Status(context.TODO(), nil)
 	assert.NoError(t, err)
 	assert.EqualValues(t, 1, ret.HealthyNodes)
-
 }
 
 func TestStatusExtensive(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	ctrl, ctx := gomock.WithContext(ctx, t)
+	ctrl := gomock.NewController(t)
 
 	// Created mocked store
 	ms := mock_store.NewMockStore(ctrl)
@@ -80,7 +77,7 @@ func TestStatusExtensive(t *testing.T) {
 	var s store.Store = ms
 	ss := statusService{&s}
 
-	ret, err := ss.Status(nil, nil)
+	ret, err := ss.Status(context.TODO(), nil)
 	assert.NoError(t, err)
 	assert.EqualValues(t, 3, ret.HealthyNodes)
 }
