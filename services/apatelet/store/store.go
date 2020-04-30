61ae47222855b9a8f65df67c13e76e494708b01d
Tim Anema <tim.anema@hotmail.nl>
2020-04-30 16:48:10 +0200

    Added initial apate store

---
+++ services/apatelet/store/store.go
@@ -0,0 +1,86 @@
+// Package store provides a way for the apatelet to have state
+package store
+
+import (
+	"container/heap"
+	"errors"
+	"sync"
+
+	"github.com/atlarge-research/opendc-emulate-kubernetes/api/apatelet"
+)
+
+// Store represents the state of the apatelet
+type Store interface {
+	// EnqueueTasks creates a priority queue based on these tasks
+	EnqueueTasks([]*apatelet.Task)
+
+	// PollTask returns the start time of the next task in the priority queue, without removing it from the queue
+	PollTask() (int64, error)
+
+	// GetTask returns the first task to be executed and removes it from the queue
+	GetTask() (*apatelet.Task, error)
+
+	// GetFlag returns the value of the given flag, default value is zero for non-existing flags
+	GetFlag(string) (int, error)
+
+	// UpdateFlag updates the value of the given flag with the given value (the value is added to the current value),
+	// or creates it with the given value if it didn't exist before
+	UpdateFlag(string, int)
+}
+
+type store struct {
+	queue    *taskQueue
+	flags    map[string]int
+	flagLock sync.RWMutex
+}
+
+// NewStore returns an empty store
+func NewStore() Store {
+	return &store{
+		flags: make(map[string]int),
+	}
+}
+
+func (s *store) EnqueueTasks(tasks []*apatelet.Task) {
+	s.queue = newTaskQueue()
+
+	for _, task := range tasks {
+		s.queue.Push(task)
+	}
+
+	heap.Init(s.queue)
+}
+
+func (s *store) PollTask() (int64, error) {
+	if s.queue.Len() == 0 {
+		return -1, errors.New("no tasks left")
+	}
+
+	return int64(s.queue.Poll().(*apatelet.Task).Timestamp), nil
+}
+
+func (s *store) GetTask() (*apatelet.Task, error) {
+	if s.queue.Len() == 0 {
+		return nil, errors.New("no tasks left")
+	}
+
+	return heap.Pop(s.queue).(*apatelet.Task), nil
+}
+
+func (s *store) GetFlag(flag string) (int, error) {
+	s.flagLock.RLock()
+	defer s.flagLock.RUnlock()
+
+	if val, ok := s.flags[flag]; ok {
+		return val, nil
+	}
+
+	return 0, errors.New("flag defaulted to zero")
+}
+
+func (s *store) UpdateFlag(flag string, val int) {
+	s.flagLock.Lock()
+	defer s.flagLock.Unlock()
+
+	s.flags[flag] += val
+}
---
+++ services/apatelet/store/task_pq.go
@@ -0,0 +1,73 @@
+package store
+
+import (
+	"sync"
+
+	"github.com/atlarge-research/opendc-emulate-kubernetes/api/apatelet"
+)
+
+// taskQueue is a thread-safe priority queue based on a min-heap for tasks in the private scenario
+type taskQueue struct {
+	tasks []*apatelet.Task
+	lock  sync.RWMutex
+}
+
+func newTaskQueue() *taskQueue {
+	return &taskQueue{
+		tasks: make([]*apatelet.Task, 0),
+	}
+}
+
+// Len is the number of elements in the queue
+func (q *taskQueue) Len() int {
+	q.lock.RLock()
+	defer q.lock.RUnlock()
+
+	return len(q.tasks)
+}
+
+// Less reports whether the element with index i should sort before the element with index j
+func (q *taskQueue) Less(i, j int) bool {
+	q.lock.RLock()
+	defer q.lock.RUnlock()
+
+	return q.tasks[i].Timestamp < q.tasks[j].Timestamp
+}
+
+// Swap swaps the elements with indexes i and j
+func (q *taskQueue) Swap(i, j int) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	q.tasks[i], q.tasks[j] = q.tasks[j], q.tasks[i]
+}
+
+// Push pushed a new task to the queue
+func (q *taskQueue) Push(x interface{}) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	task := x.(*apatelet.Task)
+	q.tasks = append(q.tasks, task)
+}
+
+// Pop returns the first task in the queue and removes it
+func (q *taskQueue) Pop() interface{} {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	old := q.tasks
+	n := len(old)
+	task := old[n-1]
+	old[n-1] = nil
+	q.tasks = old[0 : n-1]
+	return task
+}
+
+// Poll returns the first task in the queue without removing it
+func (q *taskQueue) Poll() interface{} {
+	q.lock.RLock()
+	defer q.lock.RUnlock()
+
+	return q.tasks[q.Len()-1]
+}
