6e42c690c8518791d3b78f4e51507bab879ece0e
Victor Roest <victor@xirion.net>
2020-05-02 15:55:57 +0200

    More scheduler tests

--- services/apatelet/scheduler/scheduler_test.go
+++ services/apatelet/scheduler/scheduler_test.go
@@ -10,11 +10,14 @@ import (
 	"github.com/atlarge-research/opendc-emulate-kubernetes/services/apatelet/store"
 	"github.com/atlarge-research/opendc-emulate-kubernetes/services/apatelet/store/mock_store"
 
+	"context"
+	"errors"
+	"testing"
+	"time"
+
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/protobuf/types/known/anypb"
-
-	"testing"
 )
 
 func TestTaskHandlerSimple(t *testing.T) {
@@ -118,5 +121,131 @@ func TestTaskHandlerError(t *testing.T) {
 		assert.Error(t, err)
 	default:
 		t.Fail()
+	}
+}
+
+func TestRunner(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	ms := mock_store.NewMockStore(ctrl)
+
+	a, _ := any.Marshal(scenario.Response_ERROR)
+
+	// Test task:
+	task := apatelet.Task{
+		EventFlags: map[int32]*anypb.Any{
+			events.NodeCreatePodResponse: a,
+		},
+	}
+
+	// Expectations
+	ms.EXPECT().PeekTask().Return(int64(0), nil)
+	ms.EXPECT().PopTask().Return(&task, nil)
+	ms.EXPECT().SetFlag(gomock.Any(), gomock.Any())
+
+	var s store.Store = ms
+	sched := Scheduler{&s}
+
+	// Run code under test
+	ech := make(chan error, 1)
+
+	sched.runner(ech)
+
+	select {
+	case <-ech:
+		t.Fail()
+	default:
+	}
+}
+
+func TestRunnerEarlyFail(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	ms := mock_store.NewMockStore(ctrl)
+
+	// Expectations
+	ms.EXPECT().PeekTask().Return(int64(-1), errors.New("some error"))
+
+	// Setup
+	var s store.Store = ms
+	sched := Scheduler{&s}
+
+	// Test
+	ech := make(chan error, 1)
+	sched.runner(ech)
+
+	// Verify
+	select {
+	case err := <-ech:
+		assert.Error(t, err)
+	default:
+		t.Fail()
+	}
+}
+
+func TestRunnerPopFail(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	ms := mock_store.NewMockStore(ctrl)
+
+	// Expectations
+	ms.EXPECT().PeekTask().Return(int64(0), nil)
+	ms.EXPECT().PopTask().Return(nil, errors.New("new error"))
+
+	// Setup
+	var s store.Store = ms
+	sched := Scheduler{&s}
+
+	// Test
+	ech := make(chan error, 1)
+	sched.runner(ech)
+
+	// Verify
+	select {
+	case err := <-ech:
+		assert.Error(t, err)
+	default:
+		t.Fail()
+	}
+}
+
+func TestStartScheduler(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ctrl, ctx := gomock.WithContext(ctx, t)
+
+	ms := mock_store.NewMockStore(ctrl)
+
+	a, _ := any.Marshal(scenario.Response_ERROR)
+
+	// Test task:
+	task := apatelet.Task{
+		EventFlags: map[int32]*anypb.Any{
+			events.NodeCreatePodResponse: a,
+		},
+	}
+
+	// Expectations
+	ms.EXPECT().PeekTask().Return(int64(0), nil)
+	ms.EXPECT().PopTask().Return(&task, nil)
+	ms.EXPECT().SetFlag(gomock.Any(), gomock.Any())
+
+	// any further peeks are well into the future
+	ms.EXPECT().PeekTask().Return(time.Now().Add(time.Hour*12).Unix(), nil).AnyTimes()
+
+	var s store.Store = ms
+	sched := Scheduler{&s}
+
+	// Run code under test
+	ech := make(chan error, 1)
+
+	sched.StartScheduler(ctx)
+
+	time.Sleep(time.Second)
+
+	select {
+	case <-ech:
+		t.Fail()
+	default:
 	}
 }
