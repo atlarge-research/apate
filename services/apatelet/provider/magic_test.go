dce0944d793fdfe62b44018c6b5b2a843e6f25e6
jdonszelmann <j.b.donszelmann@student.tudelft.nl>
2020-05-07 12:45:48 +0200

    Tested magic.go
    Co-authored-by: Victor Roest <victor@xirion.net>

---
+++ services/apatelet/provider/magic_test.go
@@ -0,0 +1,99 @@
+package provider
+
+import (
+	"context"
+	"github.com/atlarge-research/opendc-emulate-kubernetes/api/scenario"
+	"github.com/atlarge-research/opendc-emulate-kubernetes/pkg/scenario/events"
+	"github.com/atlarge-research/opendc-emulate-kubernetes/services/apatelet/store"
+	"github.com/atlarge-research/opendc-emulate-kubernetes/services/apatelet/store/mock_store"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMagicPodAndNodePod(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	ms := mock_store.NewMockStore(ctrl)
+
+	// vars
+	tStr := "test"
+	podName := "madjik"
+	PCPRF := events.PodCreatePodResponse
+	PCPRPF := events.PodCreatePodResponsePercentage
+
+	// Expectations
+	ms.EXPECT().GetPodFlag(podName, PCPRF).Return(scenario.Response_NORMAL, nil)
+	ms.EXPECT().GetPodFlag(podName, PCPRPF).Return(int32(100), nil)
+
+	// SOT
+	var s store.Store = ms
+
+	out, err := magicPodAndNode(magicPodNodeArgs{
+		magicArgs: magicArgs{
+			ctx:    context.TODO(),
+			p: &VKProvider{store: &s},
+			action: func() (i interface{}, err error) {
+				return tStr, nil
+			},
+		},
+		magicPodArgs:  magicPodArgs{
+			name:              podName,
+			podResponseFlag:   PCPRF,
+			podPercentageFlag: PCPRPF,
+		},
+	})
+
+	// Assert
+	assert.NoError(t, err)
+	assert.Equal(t, tStr, out)
+}
+
+func TestMagicPodAndNodeNode(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	ms := mock_store.NewMockStore(ctrl)
+
+	// vars
+	tStr := "test"
+	podName := "madjik"
+	PCPRF := events.PodCreatePodResponse
+	PCPRPF := events.PodCreatePodResponsePercentage
+
+	NCPRF := events.PodCreatePodResponse
+	NCPRPF := events.PodCreatePodResponsePercentage
+
+
+	// Expectations
+	ms.EXPECT().GetPodFlag(podName, PCPRF).Return(scenario.Response_NORMAL, nil)
+	ms.EXPECT().GetPodFlag(podName, PCPRPF).Return(int32(0), nil)
+
+	ms.EXPECT().GetNodeFlag(NCPRF).Return(scenario.Response_NORMAL, nil)
+	ms.EXPECT().GetNodeFlag(NCPRPF).Return(int32(100), nil)
+	
+	// SOT
+	var s store.Store = ms
+
+	out, err := magicPodAndNode(magicPodNodeArgs{
+		magicArgs: magicArgs{
+			ctx:    context.TODO(),
+			p: &VKProvider{store: &s},
+			action: func() (i interface{}, err error) {
+				return tStr, nil
+			},
+		},
+		magicPodArgs:  magicPodArgs{
+			name:              podName,
+			podResponseFlag:   PCPRF,
+			podPercentageFlag: PCPRPF,
+		},
+		magicNodeArgs: magicNodeArgs{
+			nodeResponseFlag:   NCPRF,
+			nodePercentageFlag: NCPRPF,
+		},
+	})
+
+	// Assert
+	assert.NoError(t, err)
+	assert.Equal(t, tStr, out)
+}
