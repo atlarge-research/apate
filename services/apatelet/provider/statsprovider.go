209d374b3ef22c13b6f20cf4f37e36f20f884e5b
Tim Anema <tim.anema@hotmail.nl>
2020-05-14 16:24:16 +0200

    Updated statsprovider and added tests for it
    Co-authored-by: Victor Roest <victor@xirion.net>

--- services/apatelet/provider/statsprovider.go
+++ services/apatelet/provider/statsprovider.go
@@ -2,6 +2,8 @@ package provider
 
 import (
 	"context"
+	"github.com/atlarge-research/opendc-emulate-kubernetes/pkg/scenario/events"
+	v1 "k8s.io/api/core/v1"
 	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -23,94 +25,95 @@ func (p *Provider) now() metav1.Time {
 
 // GetStatsSummary should return a node level statistic report
 func (p *Provider) GetStatsSummary(context.Context) (*stats.Summary, error) {
+	pods := p.getAggregatePodStats()
+
 	return &stats.Summary{
-		Node: p.getNodeStats(),
-		Pods: p.getAggregatePodStats(),
+		Node: p.getNodeStats(&pods),
+		Pods: pods,
 	}, nil
 }
 
-// TODO: Decide which we want to implement
-
 // Node statistics
-func (p *Provider) getNodeStats() stats.NodeStats {
+func (p *Provider) getNodeStats(pods *[]stats.PodStats) stats.NodeStats {
 	return stats.NodeStats{
 		NodeName:         p.nodeInfo.Name,
 		SystemContainers: []stats.ContainerStats{},
 		StartTime:        p.stats.startTime,
-		CPU:              p.cpuStats(),
-		Memory:           p.memoryStats(),
-		Network:          nil,
-		Fs:               nil,
-		Runtime:          nil,
-		Rlimit:           nil,
+		CPU:              p.cpuStats(pods),
+		Memory:           p.memoryStats(pods),
+
+		// TODO: Do we want these? They have 0 added value afaik
+		Network: nil,
+		Fs:      nil,
+		Runtime: nil,
+		Rlimit:  nil,
 	}
 }
 
-func (p *Provider) cpuStats() *stats.CPUStats {
-	cnt := uint64(532425124124)
+func (p *Provider) cpuStats(pods *[]stats.PodStats) *stats.CPUStats {
+	zero := uint64(0)
+	usage := uint64(0)
+
+	for _, pod := range *pods {
+		if pod.CPU != nil && pod.CPU.UsageNanoCores != nil {
+			usage += *pod.CPU.UsageNanoCores
+		}
+	}
+
 	return &stats.CPUStats{
 		Time:                 p.now(),
-		UsageNanoCores:       &cnt,
-		UsageCoreNanoSeconds: &cnt,
+		UsageNanoCores:       &usage,
+		UsageCoreNanoSeconds: &zero,
 	}
 }
 
-func (p *Provider) memoryStats() *stats.MemoryStats {
-	cnt := uint64(532425124124)
+func (p *Provider) memoryStats(pods *[]stats.PodStats) *stats.MemoryStats {
+	zero := uint64(0)
+	usage := uint64(0)
+
+	for _, pod := range *pods {
+		if pod.Memory != nil && pod.Memory.UsageBytes != nil {
+			usage += *pod.Memory.UsageBytes
+		}
+	}
+
+	available := uint64(p.resources.Memory) - usage
+
 	return &stats.MemoryStats{
 		Time:            p.now(),
-		AvailableBytes:  &cnt,
-		UsageBytes:      &cnt,
-		WorkingSetBytes: &cnt,
-		RSSBytes:        &cnt,
-		PageFaults:      &cnt,
-		MajorPageFaults: &cnt,
+		AvailableBytes:  &available,
+		UsageBytes:      &usage,
+		WorkingSetBytes: &zero,
+		RSSBytes:        &zero,
+		PageFaults:      &zero,
+		MajorPageFaults: &zero,
 	}
 }
 
-// Pod statistics
 func (p *Provider) getAggregatePodStats() []stats.PodStats {
-	return []stats.PodStats{}
+	var statistics []stats.PodStats
+
+	for _, pod := range p.pods.GetAllPods() {
+		statistics = append(statistics, p.getPodStats(pod))
+	}
+
+	return statistics
 }
 
-func (p *Provider) getPodStats() stats.PodStats {
-	return stats.PodStats{
-		PodRef: stats.PodReference{
-			Name:      "",
-			Namespace: "",
-			UID:       "",
-		},
-		StartTime: metav1.NewTime(time.Now()),
-		Containers: []stats.ContainerStats{
-			{
-				Name:               "",
-				StartTime:          metav1.Time{},
-				CPU:                nil,
-				Memory:             nil,
-				Accelerators:       nil,
-				Rootfs:             nil,
-				Logs:               nil,
-				UserDefinedMetrics: nil,
-			},
-		},
-		CPU:     nil,
-		Memory:  nil,
-		Network: nil,
-		VolumeStats: []stats.VolumeStats{
-			{
-				FsStats: stats.FsStats{
-					Time:           metav1.Time{},
-					AvailableBytes: nil,
-					CapacityBytes:  nil,
-					UsedBytes:      nil,
-					InodesFree:     nil,
-					Inodes:         nil,
-					InodesUsed:     nil,
-				},
-				Name:   "",
-				PVCRef: nil,
-			},
-		},
-		EphemeralStorage: nil,
+func (p *Provider) getPodStats(pod *v1.Pod) stats.PodStats {
+	for k, label := range pod.Labels {
+		if k == "apate" { //TODO: Const or something
+			event := events.PodResources
+
+			statistics, err := (*p.store).GetPodFlag(label, event)
+
+			if err != nil {
+				return stats.PodStats{PodRef: stats.PodReference{Name: pod.Name, Namespace: pod.Namespace, UID: string(pod.UID)}}
+			}
+
+			return statistics.(stats.PodStats)
+		}
 	}
+
+	return stats.PodStats{PodRef: stats.PodReference{Name: pod.Name, Namespace: pod.Namespace, UID: string(pod.UID)}}
 }
