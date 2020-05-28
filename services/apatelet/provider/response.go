e301d3351609a1f081aacda282d9b7390a317fba
Robbin Baauw <robbin.baauw@gmail.com>
2020-05-28 14:13:53 +0200

    Response refactor

--- services/apatelet/provider/response.go
+++ services/apatelet/provider/response.go
@@ -28,84 +28,101 @@ type responseArgs struct {
 	action   func() (interface{}, error)
 }
 
-func podResponse(args responseArgs, label string, responseFlag events.PodEventFlag) (interface{}, bool, error) {
-	iflag, err := (*args.provider.Store).GetPodFlag(label, responseFlag)
+func getPodResponseFlag(args responseArgs, label string, podEventFlag events.PodEventFlag) (scenario.Response, error) {
+	iflag, err := (*args.provider.Store).GetPodFlag(label, podEventFlag)
 	if err != nil {
-		return nil, false, errors.Wrap(err, "failed to get pod flag")
+		return scenario.ResponseUnset, errors.Wrapf(err, "failed to get pod flag %v", podEventFlag)
 	}
 
 	flag, ok := iflag.(scenario.Response)
 	if !ok {
-		return nil, false, errors.Errorf("couldn't cast %v to response", flag)
+		return scenario.ResponseUnset, errors.Errorf("couldn't cast %v to response", flag)
 	}
 
-	switch flag {
-	case scenario.ResponseNormal:
-		res, err := args.action()
-		return res, true, errors.Wrap(err, "failed to execute pod response action")
-	case scenario.ResponseTimeout:
-		<-args.ctx.Done()
-		return nil, true, nil
-	case scenario.ResponseError:
-		return nil, true, emulationError("podResponse expected error")
-	case scenario.ResponseUnset:
-		return nil, false, nil
-	default:
-		return nil, false, errors.New("podResponse invalid scenario")
+	return flag, nil
+}
+
+func getNodeResponseFlag(args responseArgs, nodeEventFlag events.NodeEventFlag) (scenario.Response, error) {
+	iflag, err := (*args.provider.Store).GetNodeFlag(nodeEventFlag)
+	if err != nil {
+		return scenario.ResponseUnset, errors.Wrapf(err, "failed to get node flag %v", nodeEventFlag)
 	}
+
+	flag, ok := iflag.(scenario.Response)
+	if !ok {
+		return scenario.ResponseUnset, errors.Errorf("couldn't cast %v to response", flag)
+	}
+
+	return flag, nil
 }
 
 // nodeResponse checks the passed flags and calls the passed function on success
-func nodeResponse(args responseArgs, responseFlag events.NodeEventFlag) (interface{}, error) {
-	iflag, err := (*args.provider.Store).GetNodeFlag(responseFlag)
+func nodeResponse(args responseArgs, nodeEventFlag events.NodeEventFlag) (interface{}, error) {
+	flag, err := getNodeResponseFlag(args, nodeEventFlag)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to get node flag")
-	}
-
-	flag, ok := iflag.(scenario.Response)
-	if !ok {
-		return nil, errors.Errorf("couldn't cast %v to response", flag)
+		return nil, errors.Wrap(err, "failed to retrieve node flag")
 	}
 
 	switch flag {
-	case scenario.ResponseUnset:
-		fallthrough // If unset, act as if it's normal
-	case scenario.ResponseNormal:
-		res, err := args.action()
-		return res, errors.Wrap(err, "failed to execute node response action")
 	case scenario.ResponseTimeout:
 		<-args.ctx.Done()
 		return nil, nil
 	case scenario.ResponseError:
 		return nil, emulationError("nodeResponse expected error")
+	case scenario.ResponseUnset:
+		fallthrough
+	case scenario.ResponseNormal:
+		fallthrough
 	default:
-		return nil, errors.New("nodeResponse invalid scenario")
+		res, err := args.action()
+		return res, errors.Wrap(err, "failed to execute node response action")
 	}
 }
 
-// podAndNodeResponse first calls podResponse and if pod response is not set calls nodeResponse
-func podAndNodeResponse(args responseArgs, podLabel string, podResponseFlag events.PodEventFlag, nodeResponseFlag events.NodeEventFlag) (interface{}, error) {
-	pod, performedAction, err := podResponse(args, podLabel, podResponseFlag)
+func getCorrespondingNodeEventFlag(podEventFlag events.PodEventFlag) (events.NodeEventFlag, error) {
+	switch podEventFlag {
+	case events.PodCreatePodResponse:
+		return events.NodeCreatePodResponse, nil
+	case events.PodUpdatePodResponse:
+		return events.NodeUpdatePodResponse, nil
+	case events.PodDeletePodResponse:
+		return events.NodeDeletePodResponse, nil
+	case events.PodGetPodResponse:
+		return events.NodeGetPodsResponse, nil
+	case events.PodGetPodStatusResponse:
+		return events.NodeGetPodStatusResponse, nil
+	default:
+		return 0, errors.Errorf("pod event flag %v cannot be translated into node flag", podEventFlag)
+	}
+}
 
+// podResponse determines how a pod action should respond, also based on flags on node level
+func podResponse(args responseArgs, podLabel string, podEventFlag events.PodEventFlag) (interface{}, error) {
+	podFlag, err := getPodResponseFlag(args, podLabel, podEventFlag)
 	if err != nil {
-		if IsExpected(err) {
-			return nil, err
-		}
-
-		return nil, errors.Wrap(err, "failed during pod response (not going to try node response)")
+		return nil, errors.Wrap(err, "failed to retrieve pod flag")
 	}
 
-	if performedAction {
-		return pod, nil
-	}
-
-	node, err := nodeResponse(args, nodeResponseFlag)
+	nodeEventFlag, err := getCorrespondingNodeEventFlag(podEventFlag)
 	if err != nil {
-		if IsExpected(err) {
-			return nil, err
-		}
-
-		return nil, errors.Wrap(err, "failed during pod response")
+		return nil, errors.Wrap(err, "error translating pod event flag to node event flag")
 	}
-	return node, nil
+
+	nodeFlag, err := getNodeResponseFlag(args, nodeEventFlag)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to retrieve node flag")
+	}
+
+	if podFlag == scenario.ResponseTimeout || nodeFlag == scenario.ResponseTimeout {
+		// first try for timeout
+		<-args.ctx.Done()
+		return nil, nil
+	} else if podFlag == scenario.ResponseError || nodeFlag == scenario.ResponseError {
+		// then error
+		return nil, emulationError("nodeResponse expected error")
+	} else {
+		// then normal. Unset is also normal, as is any invalid type (which shouldn't even be possible)
+		res, err := args.action()
+		return res, errors.Wrap(err, "failed to execute node response action")
+	}
 }
